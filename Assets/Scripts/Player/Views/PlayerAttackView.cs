a7658baaa999f6cb560c4e0a4548a12acc8b41f3
dovi3 <javiergarciagomez@enti.cat>
2023-01-04 17:11:25 +0100

    Fix multiple attacks at same time

--- Assets/Scripts/Player/Views/PlayerAttackView.cs
+++ Assets/Scripts/Player/Views/PlayerAttackView.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class PlayerAttackView : PlayerElement
 {
+    private const float SIMPLE_ATTACK_TIME = 0.15f;
+    private const float CIRCULAR_ATTACK_TIME = 0.5f;
+
     private GameObject simpleAttackArea;
     private GameObject circularAttackArea;
+    private bool isAttacking;
 
     private void Start()
     {
         simpleAttackArea = transform.GetChild(0).gameObject;
         circularAttackArea = transform.GetChild(1).gameObject;
+        isAttacking = false;
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !isAttacking)
         {
             SimpleAttack();
         }
 
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1) && !isAttacking)
         {
             CircularAttack();
         }
@@ -29,24 +34,28 @@ public class PlayerAttackView : PlayerElement
     private void SimpleAttack()
     {
         simpleAttackArea.SetActive(true);
+        isAttacking = true;
         StartCoroutine(DisableSimpleAttackArea());
     }
 
     IEnumerator DisableSimpleAttackArea ()
     {
-        yield return new WaitForSeconds(0.35f);
+        yield return new WaitForSeconds(SIMPLE_ATTACK_TIME);
         simpleAttackArea.SetActive(false);
+        isAttacking = false;
     }
 
     private void CircularAttack()
     {
         circularAttackArea.SetActive(true);
+        isAttacking = true;
         StartCoroutine(DisableCircularAttackArea());
     }
 
     IEnumerator DisableCircularAttackArea()
     {
-        yield return new WaitForSeconds(0.55f);
+        yield return new WaitForSeconds(CIRCULAR_ATTACK_TIME);
         circularAttackArea.SetActive(false);
+        isAttacking = false;
     }
 }
